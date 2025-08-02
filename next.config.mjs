5d7640374afb6521e571562600375de84e726618
mahdi <mahdinouri1030@gmail.com>
2025-08-02 13:53:38 +0330

    image path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['firouzehjavaherian.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'firouzehjavaherian.com',
+        pathname: '/uploads/**',
+      },
+    ],
   },
 };
 
